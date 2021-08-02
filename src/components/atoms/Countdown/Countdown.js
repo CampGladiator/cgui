139f58460ae26fa4c2829fb60c179034ea191860
websmith <n8cornelius@gmail.com>
2021-08-02 16:26:40 -0500

    [MARS-4370] update countdown component namespace and refactor reduce to map

--- src/components/atoms/Countdown/Countdown.js
+++ src/components/atoms/Countdown/Countdown.js
@@ -4,16 +4,18 @@ import './Countdown.scss'
 const diff = (a, b) => {
   const distance = a - b
 
-  return {
-    day: Math.floor(distance / (1000 * 60 * 60 * 24)),
-    hour: Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-    min: Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
-    sec: Math.floor((distance % (1000 * 60)) / 1000),
-  }
+  return [
+    { day: Math.floor(distance / (1000 * 60 * 60 * 24)) },
+    {
+      hour: Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    },
+    { min: Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)) },
+    { sec: Math.floor((distance % (1000 * 60)) / 1000) },
+  ]
 }
 
 const Countdown = ({
-  endTime = Date.now().toISOString(),
+  endTime = new Date().toISOString(),
   className = '',
   ...props
 }) => {
@@ -27,21 +29,23 @@ const Countdown = ({
 
   return (
     <figure className={`${className} countdown`} {...props}>
-      {Object.entries(difference).reduce(
-        (acc, [key, comp]) => (
+      {difference.map((entry, index) => {
+        const label = Object.keys(entry).toString()
+        const filterTime = (time) => (time > 0 ? time.padStart(2, '0') : '00')
+        const time = filterTime(Object.values(entry).toString())
+
+        return (
           <>
-            {acc}
-            {acc && <span className="countdown__separator">:</span>}
             <span className="countdown__item">
-              <span className="countdown__item-time">
-                {`${comp}`.padStart(2, '0')}
-              </span>
-              <small className="countdown__item-label">{key}</small>
+              <span className="countdown__item-time">{time}</span>
+              <small className="countdown__item-label">{label}</small>
             </span>
+            {index !== difference.length - 1 && (
+              <span className="countdown__separator"> : </span>
+            )}
           </>
-        ),
-        '',
-      )}
+        )
+      })}
     </figure>
   )
 }
